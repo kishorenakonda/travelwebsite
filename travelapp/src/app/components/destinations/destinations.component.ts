a3d6e0379d5cf5ca83e51dbd3f329e7032d48164
Kishore Kumar Balakrishnan <kishorekumar.balakrishnan@tatacommunications.com>
2021-07-03 01:17:17 +0530

    #master - added the plan a trip and destination implmentation

--- travelapp/src/app/components/destinations/destinations.component.ts
+++ travelapp/src/app/components/destinations/destinations.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../../app/services/http-service';
+
+interface DatasourceObj {
+  destinationList: Array<{ city: string, imageUrl: string }>;
+  errorMessage: string;
+}
+
+interface FlagsObj {
+  displaySuccessContent: boolean;
+  displayErrorMessage: boolean;
+}
 
 @Component({
   selector: 'app-destinations',
@@ -7,9 +18,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DestinationsComponent implements OnInit {
 
-  constructor() { }
+  datasource = {} as DatasourceObj;
+  flags = {} as FlagsObj;
+
+  constructor(private httpService: HttpService) { }
 
   ngOnInit() {
+    this.getFlagsStatus();
+    this.getDefaultDetails();
   }
 
+  getFlagsStatus() {
+    this.flags.displayErrorMessage = false;
+    this.flags.displaySuccessContent = false;
+  }
+
+  getDefaultDetails() {
+    this.fetchDestinationList();
+  }
+
+  fetchDestinationList() {
+    const url = 'https://run.mocky.io/v3/3e6901dd-9a60-4771-a8cb-9c62177a654c';
+    this.httpService.getHttp(
+      url,
+      (response) => {
+        if (response && response.result && response.result.length > 0) {
+          this.datasource.destinationList = response.result;
+
+          this.flags.displaySuccessContent = true;
+        } else {
+          this.datasource.destinationList = [];
+          this.flags.displayErrorMessage = true;
+          this.datasource.errorMessage = 'Error in Fetching Destinations';
+        }
+      },
+      (error) => {
+        console.error('<-- Error in Fetching Destination List -->', error);
+        this.flags.displayErrorMessage = true;
+        this.datasource.errorMessage = 'Error in Fetching Destinations';
+      }
+    );
+  }
 }
