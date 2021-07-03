94ab5e2fa4db65a974aa29ea864bb18bef88fb23
Kishore Kumar Balakrishnan <kishorekumar.balakrishnan@tatacommunications.com>
2021-07-03 11:58:20 +0530

    #master - added the quote implementation

--- travelapp/src/app/app.module.ts
+++ travelapp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -24,7 +25,9 @@ import { WelcomePageComponent } from './components/welcome-page/welcome-page.com
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- travelapp/src/app/components/quote/quote.component.ts
+++ travelapp/src/app/components/quote/quote.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+
+interface FlagsObj {
+  displayForm: boolean;
+  formSubmit: boolean;
+  isValidForm: boolean;
+}
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  flags = {} as FlagsObj;
 
-  constructor() { }
+  quoteForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.getFlagsStatus();
+    this.generateCreateQuoteForm();
   }
 
+  getFlagsStatus() {
+    this.flags.displayForm = false;
+    this.flags.formSubmit = false;
+    this.flags.isValidForm = false;
+  }
+
+  generateCreateQuoteForm() {
+    this.quoteForm = this.formBuilder.group({
+      name: [null, [Validators.required]],
+      mobile: [null, [Validators.required, Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')]],
+      email: [null, [Validators.required, Validators.email]]
+    });
+
+    this.flags.displayForm = true;
+  }
+
+  onSubmitForm(quoteForm) {
+    console.log("quoteForm", quoteForm);
+    this.flags.isValidForm = false;
+    this.flags.formSubmit = true;
+    if (quoteForm.valid) {
+      this.flags.isValidForm = true;
+    }
+  }
 }
